8708fb47474e922c169ca50cd99b73fd45b3220e
tyrion huang <yue.huang.enst@gmail.com>
2018-05-28 12:28:42 +0800

    add video ip

--- src/test_video.cpp
+++ src/test_video.cpp
@@ -7,15 +7,22 @@
 
 void test_video(int argc, char* argv[]) {
 	if(argc != 3) {
-		std::cout << "usage: test_video $model_path $camera_id" << std::endl;
+		std::cout << "usage: test_video $model_path $camera_ip" << std::endl;
 		exit(1); 
 	}
 
 	std::string model_path = argv[1];
-	int camera_id = atoi(argv[2]);
+	//int camera_id = atoi(argv[2]);
+    std::string camera_ip = argv[2];
+    std::string camera_stream = "rtsp://admin:Mcdonalds@" + camera_ip + ":554//Streaming/Channels/1";
 	MTCNN mm(model_path);
 
-    cv::VideoCapture camera(camera_id);
+    int counter = 0;
+    struct timeval  tv1,tv2;
+    struct timezone tz1,tz2;
+
+    //cv::VideoCapture camera(camera_id);
+    cv::VideoCapture camera(camera_stream);
 
     if (!camera.isOpened()) {
         std::cerr << "failed to open camera" << std::endl;
@@ -31,24 +38,33 @@ void test_video(int argc, char* argv[]) {
         camera >> frame;
         if (!frame.data) {
             std::cerr << "Capture video failed" << std::endl;
-            break;
+            continue;
         }
 
-		ncnn::Mat ncnn_img = ncnn::Mat::from_pixels(frame.data, ncnn::Mat::PIXEL_BGR2RGB, frame.cols, frame.rows);
-		mm.detect(ncnn_img, finalBbox);
+		if (counter % 5 == 0) {
+			gettimeofday(&tv1,&tz1);
+            ncnn::Mat ncnn_img = ncnn::Mat::from_pixels(frame.data, ncnn::Mat::PIXEL_BGR2RGB, frame.cols, frame.rows);
+            mm.detect(ncnn_img, finalBbox);
+            gettimeofday(&tv2,&tz2);
+            int total = 0;
+            for(vector<Bbox>::iterator it=finalBbox.begin(); it!=finalBbox.end();it++) {
+                if((*it).exist) {
+                    total++;
+                    cv::rectangle(frame, cv::Point((*it).x1, (*it).y1), cv::Point((*it).x2, (*it).y2), cv::Scalar(0,0,255), 2,8,0);
+                    for(int num=0;num<5;num++) {
+                        circle(frame, cv::Point((int)*(it->ppoint+num), (int)*(it->ppoint+num+5)), 3, cv::Scalar(0,255,255), -1);
+                    }
+                }
+            }
 
-    	for(vector<Bbox>::iterator it=finalBbox.begin(); it!=finalBbox.end();it++) {
-    		if((*it).exist) {
-            	cv::rectangle(frame, cv::Point((*it).x1, (*it).y1), cv::Point((*it).x2, (*it).y2), cv::Scalar(0,0,255), 2,8,0);
-            	for(int num=0;num<5;num++) {
-                	circle(frame, cv::Point((int)*(it->ppoint+num), (int)*(it->ppoint+num+5)), 3, cv::Scalar(0,255,255), -1);
-            	}
-        	}
-    	}
+            std::cout << "detected " << total << " Persons. time eclipsed: " <<  getElapse(&tv1, &tv2) << " ms" << std::endl;
+		}
 
 		imshow("face_detection", frame);
 
-    } while (QUIT_KEY != cv::waitKey(100));
+		counter++;
+
+    } while (QUIT_KEY != cv::waitKey(1));
 
 	clock_t finish_time = clock();
 	double total_time = (double)(finish_time - start_time) / CLOCKS_PER_SEC;
