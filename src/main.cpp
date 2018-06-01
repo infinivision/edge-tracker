ff6b8b7e69e80766ce62e19e1c782f8768e58595
Yue Huang <yue.huang.enst@gmail.com>
2018-06-01 18:01:06 +0800

    use OpenCV MultiTracker, but doesn't work since an optimised tracker is used

--- src/main.cpp
+++ src/main.cpp
@@ -8,34 +8,59 @@
 
 #define QUIT_KEY 'q'
 
-cv::VideoCapture getCaptureFromIndexOrIp(const char *str) {
+using namespace std;
+using namespace cv;
+
+VideoCapture getCaptureFromIndexOrIp(const char *str) {
 	 if (strcmp(str, "0") == 0 || strcmp(str, "1") == 0) {
         // use camera index
         int camera_id = atoi(str);
-        std::cout << "camera index: " << camera_id << std::endl;
-        cv::VideoCapture camera(camera_id);
+        cout << "camera index: " << camera_id << endl;
+        VideoCapture camera(camera_id);
 		return camera;
     } else {
-        std::string camera_ip = str;
-        std::cout << "camera ip: " << camera_ip << std::endl;
-        std::string camera_stream = "rtsp://admin:Mcdonalds@" + camera_ip + ":554//Streaming/Channels/1";
-        cv::VideoCapture camera(camera_stream);
+        string camera_ip = str;
+        cout << "camera ip: " << camera_ip << endl;
+        string camera_stream = "rtsp://admin:Mcdonalds@" + camera_ip + ":554//Streaming/Channels/1";
+        VideoCapture camera(camera_stream);
 		return camera;
-    }	
+    }
+}
+
+/*
+ * Decide whether the detected face is same as the tracking one
+ * 
+ * return true when:
+ *   center point of one box is inside the other
+ */
+bool isSameFace(Rect2d &box1, Rect2d &box2) {
+	int x1 = box1.x + box1.width/2;
+	int y1 = box1.y + box1.height/2;
+	int x2 = box2.x + box2.width/2;
+	int y2 = box2.y + box2.height/2;
+
+	if ( x1 > box2.x && x1 < box2.x + box2.width &&
+		 y1 > box2.y && y1 < box2.y + box2.height &&
+		 x2 > box1.x && x2 < box1.x + box1.width &&
+		 y2 > box1.y && y2 < box1.y + box1.height ) {
+			return true;
+	}
+
+	return false;
 }
 
 void test_video(int argc, char* argv[]) {
 	if(argc != 3) {
-		std::cout << "usage: main $model_path $camera_ip" << std::endl;
+		cout << "usage: main $model_path $camera_ip" << endl;
 		exit(1); 
 	}
 
-	std::string model_path = argv[1];
+	string model_path = argv[1];
 	MTCNN mm(model_path);
 
-	cv::VideoCapture camera = getCaptureFromIndexOrIp(argv[2]);
+	VideoCapture camera = getCaptureFromIndexOrIp(argv[2]);
     if (!camera.isOpened()) {
-        std::cerr << "failed to open camera" << std::endl;
+        cerr << "failed to open camera" << endl;
         return;
     }
 
@@ -43,24 +68,24 @@ void test_video(int argc, char* argv[]) {
     struct timeval  tv1,tv2;
     struct timezone tz1,tz2;
 
-	std::vector<Bbox> finalBbox;
-    std::vector<cv::Ptr<cv::Tracker>> trackers;
-	cv::Rect2d roi;
-	cv::Mat frame;
+	vector<Bbox> finalBbox;
+    MultiTracker trackers;
+	Rect2d roi;
+	Mat frame;
 
-	cv::namedWindow("face_detection", cv::WINDOW_NORMAL);
+	namedWindow("face_detection", WINDOW_NORMAL);
 
    	do {
 		finalBbox.clear();
         camera >> frame;
         if (!frame.data) {
-            std::cerr << "Capture video failed" << std::endl;
+            cerr << "Capture video failed" << endl;
             continue;
         }
 
 		if (counter % 25 == 0) {
 			// renew trackers
-			trackers.clear();
+			//trackers.clear();
 
 			gettimeofday(&tv1,&tz1);
             ncnn::Mat ncnn_img = ncnn::Mat::from_pixels(frame.data, ncnn::Mat::PIXEL_BGR2RGB, frame.cols, frame.rows);
@@ -77,34 +102,43 @@ void test_video(int argc, char* argv[]) {
                         //circle(frame, cv::Point((int)*(it->ppoint+num), (int)*(it->ppoint+num+5)), 3, cv::Scalar(0,255,255), -1);
                     //}
 					
-					// create tracker
+					// get face bounding box
 					auto box = *it;
-					cv::Rect2d roi(cv::Point(box.x1, box.y1),cv::Point(box.x2, box.y2));
+					Rect2d detectedFace(Point(box.x1, box.y1),Point(box.x2, box.y2));
+
+					// test whether is a new face
+					bool newFace = true;
+					for (unsigned i=0;i<trackers.getObjects().size();i++) {
+						Rect2d trackerFace = trackers.getObjects()[i];
+						if (isSameFace(detectedFace, trackerFace)) {
+							newFace = false;
+							break;
+						}
+					}
 	
-					cv::Ptr<cv::Tracker> tracker = cv::TrackerKCF::create();
-					tracker->init(frame,roi);
-					trackers.push_back(tracker);
+					// create a tracker if a new face is detected
+					if (newFace) {
+						Ptr<Tracker> tracker = TrackerKCF::create();
+						trackers.add(tracker, frame, detectedFace);
+					}
                 }
             }
 
-            std::cout << "detected " << total << " Persons. time eclipsed: " <<  getElapse(&tv1, &tv2) << " ms" << std::endl;
+            cout << "detected " << total << " Persons. time eclipsed: " <<  getElapse(&tv1, &tv2) << " ms" << endl;
 		}
 
-		for (auto it = trackers.begin(); it != trackers.end(); it++) {
-			auto loss = (*it)->update(frame,roi);
-        	if (!loss) {
-            	std::cout << "Stop the tracking process" << std::endl;
-            	// break;
- 				continue;
-        	}
-			cv::rectangle( frame, roi, cv::Scalar( 255, 0, 0 ), 2, 1 );
-        }
+		// update trackers
+		trackers.update(frame);
+
+		// draw tracked faces
+		for(unsigned i=0;i<trackers.getObjects().size();i++)
+        	rectangle( frame, trackers.getObjects()[i], Scalar( 255, 0, 0 ), 2, 1 );
 
 		imshow("face_detection", frame);
 
 		counter++;
 
-    } while (QUIT_KEY != cv::waitKey(1));
+    } while (QUIT_KEY != waitKey(1));
 }
 
 int main(int argc, char* argv[]) {
