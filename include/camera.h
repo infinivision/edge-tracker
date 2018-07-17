1c770651a8687d39a4430f154269256638f24d88
Yue Huang <yue.huang.enst@gmail.com>
2018-07-17 16:50:36 +0800

    allow main program to carry multiple cameras

--- include/camera.h
+++ include/camera.h
@@ -1,7 +1,10 @@
 #ifndef __CAMERA_H__
 #define __CAMERA_H__
 
+#include "cpptoml.h"
 #include <opencv2/opencv.hpp>
+#include <glog/logging.h>
+#include <vector>
 
 class CameraConfig {
 
@@ -11,36 +14,17 @@ public:
     std::string username;
     std::string password;
     int resize_rows, resize_cols;
+    int detection_period;
 
-    CameraConfig() {};
-    CameraConfig(int index, int resize_rows, int resize_cols): index(index), resize_rows(resize_rows), resize_cols(resize_cols) {};
-    CameraConfig(std::string ip, std::string username, std::string password, int resize_rows, int resize_cols): ip(ip), username(username), password(password), resize_rows(resize_rows), resize_cols(resize_cols) {};
+    CameraConfig(){};
 
     // return ip, or index if no ip is given
-    inline std::string identity() {
-        if (ip.empty()) {
-        	return std::to_string(index);
-        } else {
-        	return ip;
-        }
-    }
+    std::string identity() const;
 
-    /*
-     * Get video capture from a camera index (0, 1) or an ip (192.168.1.15)
-     */
-    inline cv::VideoCapture GetCapture() const {
-        if ( this->ip.empty()) {
-            // use camera index
-            std::cout << "camera index: " << this->index << std::endl;
-            cv::VideoCapture capture(this->index);
-            return capture;
-        } else {
-            std::cout << "camera ip: " << this->ip << std::endl;
-            std::string camera_stream = "rtsp://" + this->username +  ":" + this->password + "@" + this->ip + ":554//Streaming/Channels/1";
-            cv::VideoCapture capture(camera_stream);
-            return capture;
-        }
-    }
+    // Get video capture from a camera index (0, 1) or an ip (192.168.1.15)
+    cv::VideoCapture GetCapture() const;
 };
+
+std::vector<CameraConfig> LoadCameraConfig(std::string config_path);
 
 #endif
---
+++ src/camera.cpp
@@ -0,0 +1,70 @@
+#include <camera.h>
+
+std::string CameraConfig::identity() const {
+	if (ip.empty()) {
+        return std::to_string(index);
+    } else {
+        return ip;
+    }
+}
+
+/*
+ * Get video capture from a camera index (0, 1) or an ip (192.168.1.15)
+ */
+cv::VideoCapture CameraConfig::GetCapture() const {
+    if ( this->ip.empty()) {
+        // use camera index
+        LOG(INFO) << "camera index: " << this->index;
+        cv::VideoCapture capture(this->index);
+        return capture;
+    } else {
+        LOG(INFO) << "camera ip: " << this->ip << std::endl;
+        std::string camera_stream = "rtsp://" + this->username +  ":" + this->password + "@" + this->ip + ":554//Streaming/Channels/1";
+        cv::VideoCapture capture(camera_stream);
+        return capture;
+    }
+}
+
+/*
+ * parse toml configuration using cpptoml library [https://github.com/skystrife/cpptoml]
+ */
+std::vector<CameraConfig> LoadCameraConfig(std::string config_path) {
+
+	std::vector<CameraConfig> cameras;
+
+	try {
+        std::shared_ptr<cpptoml::table> g = cpptoml::parse_file(config_path);
+
+        auto array = g->get_table_array("camera");
+        for (const auto &table : *array) {
+        	CameraConfig camera;
+        	auto index_ptr = table->get_as<int>("index");
+            auto ip = table->get_as<std::string>("ip").value_or("");
+            auto username_ptr = table->get_as<std::string>("username");
+            auto password_ptr = table->get_as<std::string>("password");
+            auto resize_rows = table->get_as<int>("resize_rows").value_or(0);
+            auto resize_cols = table->get_as<int>("resize_cols").value_or(0);
+            auto detection_period = table->get_as<int>("detection_period").value_or(1);
+
+            camera.resize_rows = resize_rows;
+            camera.resize_cols = resize_cols;
+            camera.detection_period = detection_period;
+
+            if (ip.empty()) {
+                camera.index = *index_ptr;
+            } else {
+                camera.ip = ip;
+                camera.username = *username_ptr;
+                camera.password = *password_ptr;
+            }
+
+            cameras.push_back(camera);
+        }
+    }
+    catch (const cpptoml::parse_exception& e) {
+        std::cerr << "Failed to parse " << config_path << ": " << e.what() << std::endl;
+        exit(1);
+    }
+
+    return cameras;
+}
